f52060e6c87c26121ef914224dced7aa6beb71a3
mojtaba <you@example.com>
2025-03-14 21:47:30 +0330

    Add: install containerd , reset kubespray

---
+++ Config/ansible/README.md
@@ -0,0 +1,2 @@
+1- create role for preparing server
+2- add ssh-key to all server
--- Config/vagrant/bootstrap.sh
+++ Config/vagrant/bootstrap.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-# update and upgrade os
+
+# Update /etc/hosts file
+echo -e "192.168.1.3\trepo.tavakolzadeh.ir" | sudo tee -a /etc/hosts
+echo -e "192.168.1.100\tmaster-node1" | sudo tee -a /etc/hosts
+echo -e "192.168.1.101\tworker-node1" | sudo tee -a /etc/hosts
+echo -e "192.168.1.102\tworker-node2" | sudo tee -a /etc/hosts
+
+# Update /etc/apt/sources.list
+echo -e "
+deb [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm main contrib non-free non-free-firmware
+deb-src [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm main contrib non-free non-free-firmware
+
+deb [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb-src [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm-updates main contrib non-free non-free-firmware
+
+deb [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb-src [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm-backports main contrib non-free non-free-firmware
+
+deb [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian-security/ bookworm-security main contrib non-free non-free-firmware
+deb-src [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian-security/ bookworm-security main contrib non-free non-free-firmware
+" | sudo tee /etc/apt/sources.list
+
 sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get autoremove -y
-#
-## add my ssh key on root user
-#sudo curl -fsSL https://store.dockerme.ir/Software/public_keys -o /root/.ssh/authorized_keys
-#
-## copy bashrc
-#sudo cp /home/vagrant/.bashrc /root/
-#
-## install docker
-#which docker || { curl -fsSL https://get.docker.com | bash; }
-#sudo usermod -aG docker $USER
-#sudo usermod -aG docker vagrant
+
+# Disable swap
+sudo swapoff -a
+sudo sed -i '/swap/d' /etc/fstab
