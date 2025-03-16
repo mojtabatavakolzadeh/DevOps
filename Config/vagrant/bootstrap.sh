a65f607935779e0d49f6e95e6a901c8c14d8e55a
mojtaba <you@example.com>
2025-03-16 11:31:55 +0330

    Edit: log_download , bootstrap.sh vagrant , increase memory vms

--- Config/vagrant/bootstrap.sh
+++ Config/vagrant/bootstrap.sh
@@ -6,6 +6,10 @@ echo -e "192.168.1.100\tmaster-node1" | sudo tee -a /etc/hosts
 echo -e "192.168.1.101\tworker-node1" | sudo tee -a /etc/hosts
 echo -e "192.168.1.102\tworker-node2" | sudo tee -a /etc/hosts
 
+# Add keyring kubernetes
+curl --proxy 192.168.1.3:2334 -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo -e "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] http://repo.tavakolzadeh.ir/repository/pkg-k8s/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list 
+
 # Update /etc/apt/sources.list
 echo -e "
 deb [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian/ bookworm main contrib non-free non-free-firmware
