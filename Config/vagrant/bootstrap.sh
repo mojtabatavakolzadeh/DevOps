2a284c0ad5837044ee23553693ee21117681933d
mojtaba <you@example.com>
2025-03-17 17:26:57 +0330

    Edit: Change of repositories

--- Config/vagrant/bootstrap.sh
+++ Config/vagrant/bootstrap.sh
@@ -2,13 +2,14 @@
 
 # Update /etc/hosts file
 echo -e "192.168.1.3\trepo.tavakolzadeh.ir" | sudo tee -a /etc/hosts
+echo -e "192.168.1.3\tregistry.tavakolzadeh.ir" | sudo tee -a /etc/hosts
 echo -e "192.168.1.100\tmaster-node1" | sudo tee -a /etc/hosts
 echo -e "192.168.1.101\tworker-node1" | sudo tee -a /etc/hosts
 echo -e "192.168.1.102\tworker-node2" | sudo tee -a /etc/hosts
 
 # Add keyring kubernetes
-curl --proxy 192.168.1.3:2334 -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo -e "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] http://repo.tavakolzadeh.ir/repository/pkg-k8s/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list 
+# curl --proxy 192.168.1.3:2334 -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# echo -e "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] http://repo.tavakolzadeh.ir/repository/pkg-k8s/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list 
 
 # Update /etc/apt/sources.list
 echo -e "
@@ -26,6 +27,8 @@ deb-src [trusted=yes] http://repo.tavakolzadeh.ir/repository/debian-security/ bo
 " | sudo tee /etc/apt/sources.list
 
 sudo apt-get update
+# Install packages
+sudo apt-get install vim -y
 
 # Disable swap
 sudo swapoff -a
--- Tools/nexus/README.md
+++ Tools/nexus/README.md
@@ -2,3 +2,5 @@
 درست کردن ایمیج می نی ای او
 لیست همه ریپازیتوری ها را به نکسوس اضافه کنم
 وصل کردن نکسوس به می نی ای او
+set row proxy for binary component kubernetes
+write script create repositories
